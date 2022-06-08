8eb1eababf28e10eed6c22c5020c744f0b1829f2
Ashley Smith <ashlynn4567@gmail.com>
2022-06-08 15:05:34 -0500

    restructure and refactor controller.js, index.js, and server.js to better follow the principle of separation of concerns

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,45 +1,43 @@
+// install dependencies
 const express = require("express");
 const router = express.Router();
 const { v4: uuidv4 } = require('uuid');
-const {
-    readNote,
-    createNote,
-    deleteNote
-} = require("../../lib/controller");
+const controller = require("../../lib/controller");
 
+// return all notes 
 router.get("/notes", (req, res) => {
-    fs.readFile("db/db.json", "utf-8", (err, data) => {
-        if (err) {
-            // res.sendStatus(400).json({ error: err.message });
-            throw err;
-        };
-        res.send(JSON.parse(data));
-    });
-});
-
-router.post("/notes", (req, res) => {
-    fs.readFile("db/db.json", "utf-8", (err, data) => {
-        if (err) {
-            res.sendStatus(400).json({ error: err.message });
-        };
-        
-        const notes = JSON.parse(data);
-        const newNote = req.body;
-        newNote.id = uuidv4();
-        notes.push(newNote);
-
-        fs.writeFile("db/db.json", JSON.stringify(notes), (err) => {
-            if (err) {
-                res.sendStatus(400).json({ error: err.message });
-            };
-            res.json(newNote);
+    controller
+        .getNotes(req.body)
+        .then((notes) => {
+            return res.json(notes);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
         });
-    });
 });
 
+// create and save a new note
+router.post("/notes", (req, res) => {
+    controller
+        .addNotes(req.body)
+        .then((note) => {
+            return res.json(note);
+        })
+        .catch((err) => {
+            return res.status(500).json(err);
+        });
+});
+
+// delete a note
 router.delete(`/notes/:id`, (req, res) => {
-    deleteNote(req.params.id, savedNotes);
-    res.json(savedNotes);
+    controller
+        .deleteNotes(req.params.id)
+        .then(() => {
+            return res.status(200).json({ message: "Note deleted successfully" });
+        })
+        .catch((err) => {
+            return res.status(500).json(err);
+        });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,17 +4,22 @@ const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
 // set up port and instantiate server
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // set up middleware
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 // setup api routes
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
+
+// error handling for invalid request
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 // tell the server to listen for requests on active port
 app.listen(PORT, () => {
